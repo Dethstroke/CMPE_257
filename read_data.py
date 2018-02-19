b96392b1257e3ceff2a6524c1a7b53fb76adb6b9
Hugo Quiroz <hugo.quiroz92@gmail.com>
2018-02-18 21:43:19 -0800

    finding mean alcohol content

--- read_data.py
+++ read_data.py
@@ -3,7 +3,30 @@ import pandas as pd
 
 
 
-df = pd.read_csv('wine_data.csv', names = ['Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of Ash', 'Magnesium', 'Total Phenols', 'Flavanoids', 'NonFalvanid Phenols', 'Proanthocyanins', 'Color Intensity', 'Hue', 'OD280/oD315 of diluted wines', 'Proline' ])
+df = pd.read_csv('wine_data.csv', index_col=False,names = ['cultivar','Alcohol', 'Malic Acid', 'Ash', 'Alcalinity of Ash', 'Magnesium', 'Total Phenols', 'Flavanoids', 'NonFalvanid Phenols', 'Proanthocyanins', 'Color Intensity', 'Hue', 'OD280/oD315 of diluted wines', 'Proline' ])
 
 
-print(df)
+new_df = df.loc[df['cultivar'] == 1]
+
+alcohol_mean = new_df['Alcohol'].mean()
+
+print('Cultivar 1 Alchohol Mean: ' + str(alcohol_mean))
+
+
+new_df = df.loc[df['cultivar'] == 2]
+
+alcohol_mean = new_df['Alcohol'].mean()
+
+print('Cultivar 2 Alchohol Mean: ' + str(alcohol_mean))
+
+new_df = df.loc[df['cultivar'] == 3]
+
+pd.new_df.plot
+
+alcohol_mean = new_df['Alcohol'].mean()
+
+print('Cultivar 3 Alchohol Mean: ' + str(alcohol_mean))
+
+
+
+
